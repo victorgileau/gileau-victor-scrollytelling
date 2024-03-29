54e18dc3112596b7d02073512218a4bac0523ace
victorgileau <vgileau@hotmail.com>
2024-03-29 17:20:21 -0400

    P2-2a_spriteSheet-debut-test-scrollTrigger

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+//Index
+
 gsap.from(['.fleche-down'], {
     duration: 0.8,
     opacity: 0,
@@ -5,4 +7,40 @@ gsap.from(['.fleche-down'], {
     ease: 'slow',
     stagger: {
         each: 0.5,
-    }});
+    },
+});
+
+
+
+//SrpiteSheet
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.all',
+        start: 'top center',
+        end: '+=1000px',
+        markers: true,
+        scrub: true,
+        pin: true,
+        toggleActions: 'restart pause reverse pause',
+    },
+})
+
+tl.to(['.sprite'], {
+    y: '-30px',
+    ease: 'none',
+    duration: 1,
+})
+.to(['.sprite'], {
+    y: '10px',
+    backgroundColor: 'red',
+    ease: 'none',
+    duration: 1,
+})
+.to(['.sprite'], {
+    backgroundColor: 'rgba(0,0,0,0)',
+    ease: 'none',
+    duration: 1,
+})
