fa007ad48eb5813b456c8037b0cd37dec802f41b
victorgileau <vgileau@hotmail.com>
2024-04-16 11:08:59 -0400

    P2-2b_debut-animation-chapitre1

--- script/script.js
+++ script/script.js
@@ -2,6 +2,35 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+const chap1SpriteStand = document.querySelector('#chapitre1 .sprite');
+const chapitre1 = document.querySelector('#chapitre1');
+const bodyIndex = document.querySelector('body');
+
+gsap.timeline().from(['.fleche-down'], {
+    duration: 0.8,
+    opacity: 0,
+    repeat: -1,
+    ease: 'slow',
+    stagger: {
+        each: 0.5,
+    },
+}).to(chap1SpriteStand, {
+    x: '80vw',
+    duration: 5,
+    onStart: () => {
+        bodyIndex.classList.add('is-scrolling')
+        chap1SpriteStand.classList.remove('standJ');
+        chap1SpriteStand.classList.add('walkJ');
+        chapitre1.classList.add('bgMove');
+    },
+    onComplete: () => {
+        chapitre1.classList.add('bgMoveStop');
+    }
+    }, '-=0.5'
+).to('#chapitre1 .sol-1', { scrub: 1,x: '-500px', duration: 3}, '-=5')
+.to('#chapitre1 .roche', { scrub: 1,x: '-10vw', duration: 3}, '-=2' )
+
+/*
 gsap.from(['.fleche-down'], {
     duration: 0.8,
     opacity: 0,
@@ -12,11 +41,10 @@ gsap.from(['.fleche-down'], {
     },
 });
 
-const tlChap1 = gsap.timeline({
-}).to('#chapitre1.sprite.standJ', { x: '90%', duration: 5});
 
-tlChap1.play();
 
+gsap.to(chap1SpriteStand, { x: '100px', duration: 5});
+*/
 /*
 gsap.utils.toArray('section').forEach((chap) => {
     gsap.timeline({
@@ -62,20 +90,24 @@ tl.to(['.sprite'], {
 
 //SrpiteSheet
 
+const all = document.querySelector('.all');
 const sprite = document.querySelectorAll('.sprite');
-const body = document.querySelector('body');
 
-window.addEventListener('scroll', () => {
-  let y = window.scrollY;
-  body.classList.add('is-scrolling');
-  setTimeout(removeClassIsScrolling, 100);
-});
-
-function removeClassIsScrolling() {
-    body.classList.remove('is-scrolling');
+if (all != null) {
+    const body = document.querySelector('body');
+    window.addEventListener('scroll', () => {
+        let y = window.scrollY;
+        body.classList.add('is-scrolling');
+        setTimeout(removeClassIsScrolling, 100);
+      });
+      
+      function removeClassIsScrolling() {
+          body.classList.remove('is-scrolling');
+      }
+      
+      function addClassIsScrolling() {
+          body.classList.add('is-scrolling');
+      }
 }
 
-function addClassIsScrolling() {
-    body.classList.add('is-scrolling');
-}
 
